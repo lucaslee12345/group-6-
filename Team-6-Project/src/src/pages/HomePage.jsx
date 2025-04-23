c1a56d188074304b741ed6362a2cb5fe44fb01ce
ProjectNerva <hedge.heisenberg@gmail.com>
2025-04-23 10:40:38 -0700

    HomePage First Push

---
+++ Team-6-Project/src/src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import '../css/HomePage.css'
+
+import homeBG from '../img/doctorWorking.jpg'
+
+function HomePage() {
+    return (
+        <>
+            <img id="topImg" src={homeBG}/>
+            {/*  */}
+        </>
+    )
+}
+
+export default HomePage;
